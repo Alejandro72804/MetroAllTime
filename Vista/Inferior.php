4509b2530a8f8d0d929ce07dc711db4e80e949ac
Alejandro72804 <lmestrabernal38@correo.unicordoba.edu.co>
2023-12-09 22:38:06 -0500

    Version1

---
+++ Vista/Inferior.php
@@ -0,0 +1,11 @@
+</body>
+<footer>
+  <div>
+    <h6>
+      <p> <small>
+          <blockquote>©2023 Made in Montería, Developed by Camilo Aparicio, Yesith Cantillo & Luis Mestra</blockquote>
+        </small></p>
+    </h6>
+  </div>
+</footer>
+</html>
--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 function conectar(){
-    $host="localhost";
-    $user="root";
-    $pass="";
-    $bd="gym";
+    $host="34.130.205.158";
+    $user="master";
+    $pass="prueba";
+    $bd="MetroAllTime";
 
     $con=mysqli_connect($host,$user,$pass);
     mysqli_select_db($con,$bd);
